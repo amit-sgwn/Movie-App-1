9dfb7c51f3d3aec77dd4e8070865be60d24f116b
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-05-07 01:22:42 +0530

    Added File for movie with parcable

--- app/src/main/java/com/mystoryapp/sangwan/movieapp1/MainActivityFragment.java
+++ app/src/main/java/com/mystoryapp/sangwan/movieapp1/MainActivityFragment.java
@@ -1,15 +1,28 @@
 package com.mystoryapp.sangwan.movieapp1;
 
+import android.content.Intent;
+import android.graphics.*;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private GridView gridView;
+    String moviesJsonStr = null;
+    static String orderBy;
+    ArrayList<String> movieIdArray = new ArrayList<String>();
 
     public MainActivityFragment() {
     }
@@ -17,6 +30,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+        gridView = (GridView) rootView.findViewById(R.id.gridview);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), android.graphics.Movie.class);
+                intent.putExtra("id",movieIdArray.get(position));
+                startActivity(intent);
+            }
+        });
+        return rootView ;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    class FatchMovieTask extends AsyncTask(){
+
     }
 }
